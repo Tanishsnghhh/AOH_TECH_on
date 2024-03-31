71b30938f0133ecd37941b439e0daed7d3bd22ce
Tanish <142775471+Tanishsnghhh@users.noreply.github.com>
2024-03-31 12:08:17 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# AOH_TECH_on
+# Diet planner
+
+A new Flutter project.
+
+## Getting Started
+
+FlutterFlow projects are built to run on the Flutter _stable_ release.
---
+++ lib/index.dart
@@ -0,0 +1,12 @@
+// Export pages
+export '/pages/registraionpage/registraionpage_widget.dart'
+    show RegistraionpageWidget;
+export '/chat_g_p_t_component/myprofile/myprofile_widget.dart'
+    show MyprofileWidget;
+export '/pages/patient_intakemain/patient_intakemain_widget.dart'
+    show PatientIntakemainWidget;
+export '/pages/weightaccuracy/weightaccuracy_widget.dart'
+    show WeightaccuracyWidget;
+export '/pages/selectfoodpref/selectfoodpref_widget.dart'
+    show SelectfoodprefWidget;
+export '/pages/gpt/gpt_widget.dart' show GptWidget;
